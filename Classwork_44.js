a44ec47d5c04c028bc491f479a85700b5aeb6f16
rayna016 <102683642+rayna016@users.noreply.github.com>
2022-04-13 11:48:14 -0500

    Add files via upload

---
+++ Classwork_44.js
@@ -0,0 +1,39 @@
+// OBJECTIVE: Get practice with JavaScript Objects and learn how to make them and access them.
+
+// 1. Make an object called "house" with the following properties (name/value pairs):
+// a) numberOfRooms (data-type = number)
+// b) numberOfBathrooms (data-type = number)
+// c) hasGarage (data-type = boolean)
+// d) listOfRooms (data-type = array with strings)
+// e) hasStairs (data-type = boolean)
+let house = {
+    numberOfRooms: 5,
+    numberOfBathrooms: 2,
+    HasGarage: true,
+    listOfRooms: ["living room" , "Bedrooms" , "Guest Bedroom" , "Kitchen"],
+    hasStairs : false,
+}
+
+// 2. Print out (output) the number of rooms it has.
+console.log(house.numberOfRooms);
+
+// 3. Print out if it has a garage or not.
+console.log(house.HasGarage);
+
+// 4. Print out the list of rooms.
+console.log(house.listOfRooms)
+// 5. Make a function that will take in an array as a parameter
+// and will loop through that array to check for duplicates. If the
+// list has a duplicate at least return true, otherwise return false.
+
+function findDuplicate () {
+    let array= {num1, num2};
+    let n = sizeof(arr) / sizeof(arr[0]);
+    if (checkDuplicatesWithinK(num1, num2, num3))
+       true;
+    else
+        false;
+}
+    
+
+
